da9d46c92ee80a3ca838b700ce3db066fd3cb8e4
Tejas Kale <tejas.kale@tum.de>
2017-11-20 18:01:35 +0100

    Added animations, relaxation factor

--- 2/art/art.py
+++ 2/art/art.py
@@ -1,8 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as pp
+import time
 
 
-def art(A, b, iterations):
-
+def art(A, b, iterations, relax):
     # For help with numpy (the numerical programming library for Python) check out this resource:
     # https://www.safaribooksonline.com/library/view/python-for-data/9781449323592/ch04.html
     x = np.zeros((A.shape[1]))
@@ -23,6 +24,32 @@ def art(A, b, iterations):
             if sqrd_norm <= 0:
                 continue
             else:
-                x += (b[j][0] - np.dot(row, x)) * np.conjugate(row) / sqrd_norm
+                x += relax * ((b[j][0] - np.dot(row, x)) * np.conjugate(row) / sqrd_norm)
             
+    return x
+
+def art_anim(A, b, iterations, relax):
+
+    x = np.zeros((A.shape[1]))
+
+    fig,ax = pp.subplots(1,1)
+
+    image = ax.imshow(np.random.rand(60,60),cmap='bone',animated=True)
+    fig.canvas.draw()
+    fig.show();
+
+    for it in range(iterations):
+        # Iterate over matrix rows
+        for j, row in enumerate(A):
+            sqrd_norm = (np.linalg.norm(row) ** 2)
+            # x' = x + correction
+            if sqrd_norm <= 0:
+                continue
+            else:
+                x += relax * ((b[j][0] - np.dot(row, x)) * np.conjugate(row) / sqrd_norm)
+
+        image.set_data(x.reshape((60,60)))
+        ax.set_title(it)
+        fig.canvas.draw()
+        # time.sleep(0.05)
     return x
