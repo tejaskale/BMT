e5e102772463a8566e6f014ca80ee52d50c5bfbb
Tejas Kale <tejas.kale@tum.de>
2017-11-21 01:37:53 +0100

    Improve prints.

--- 3/qr.py
+++ 3/qr.py
@@ -1,5 +1,6 @@
 import numpy as np
 import timeit
+import pprint
 
 def projection(u, x): return (np.dot(x, u) / np.dot(u, u)) * u
 
@@ -77,7 +78,8 @@ elapsed = timeit.default_timer() - start_time
 start_time = timeit.default_timer()
 q, r = np.linalg.qr(A)
 print('NUMPY')
-print(q, r)
+pprint.pprint(q)
+pprint.pprint(r)
 y = np.matmul(q.T, b)
 x_qr = np.linalg.solve(r, y)
 elapsed = timeit.default_timer() - start_time
@@ -86,7 +88,9 @@ print('using numpy qr : {}, took {} seconds'.format(x_qr, elapsed))
 start_time = timeit.default_timer()
 q2, r2 = my_qr(A)
 print('OWN')
-print(q2, r2)
+pprint.pprint(q)
+pprint.pprint(r)
+
 y2 = np.matmul(q2.T, b)
 x_qr2 = np.linalg.solve(r2, y2)
 elapsed = timeit.default_timer() - start_time
